94ab4d8423e261684ff22970af4155d7ae0ecf86
Mohammad Saif <saifsami321md@gmail.com>
2022-11-26 00:51:42 +0530

    component has been created as well as db.json also created

--- redux_todo/src/App.js
+++ redux_todo/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Todos from './Component/Todos';
 
 function App() {
   return (
     <div className="App">
       <h1>Basic SetUp</h1>
+      <Todos/>
     </div>
   );
 }
---
+++ redux_todo/src/Component/TodoInput.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const TodoInput = () => {
+    <div>
+        <h1>TodoInput</h1>
+    </div>
+}
+
+export default TodoInput
+
